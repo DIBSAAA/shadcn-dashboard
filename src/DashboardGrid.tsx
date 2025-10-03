7aa3df4310ba968db20322c8e0d7d0d0ffaf2f1d
Dibyawan Trivedi <dibyawan.trivedi@geotechinfo.net>
2025-10-03 18:44:50 +0530

    init: adding a new grid to the main code pt2

--- src/DashboardGrid.tsx
+++ src/DashboardGrid.tsx
@@ -1,18 +1,27 @@
-import { GridStack } from 'gridstack';
+import { BarChartMixed } from './components/bar-chart-mixed';
+import { ChartAreaInteractive } from './components/chart-area-interactive';
+import { PieChartCard } from './components/pie-chart-card';
+import { RadarChartIcons } from './components/radar-chart-icons';
+import { SectionCards } from './components/section-cards';
 
 function DashboardGrid() {
-
-    GridStack.init();
-
   return (
-    <div className="grid-stack">
-  <div className="grid-stack-item">
-    <div className="grid-stack-item-content">Item 1</div>
-  </div>
-  <div className="grid-stack-item" gs-w="2">
-    <div className="grid-stack-item-content">Item 2 wider</div>
-  </div>
-</div>
+    <div className="dashboard">
+      <div className='border-0 border-b-2 mb-4 pb-4 border-card-foreground'>
+        <SectionCards />
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+        <RadarChartIcons />
+        <BarChartMixed />
+        <PieChartCard />
+        <div className='col-span-1 lg:col-span-3'>
+          <ChartAreaInteractive />
+        </div>
+        <PieChartCard />
+        <RadarChartIcons />
+        <BarChartMixed />
+      </div>
+    </div>
   )
 }
 
